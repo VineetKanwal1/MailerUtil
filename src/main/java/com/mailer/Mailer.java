e348ab01e0c4d0c09de1509407c9119bd2084d76
Vineet K Sabharwal <vikanwal@in.ibm.com>
2022-03-20 22:31:19 +0530

    balance confirmation email changes

--- src/main/java/com/mailer/Mailer.java
+++ src/main/java/com/mailer/Mailer.java
@@ -11,8 +11,8 @@ import javax.mail.internet.*;
 
 public class Mailer {
 
-	public static void send(final String from, final String password, final String to, final String sub,
-			final String msg, final String pdfs, final String pdfPath) {
+	public static void send(final String from, final String password, final String[] to, final String sub,
+			final String msg, final String pdf, final String pdfPath) throws MessagingException {
 		// Get properties object
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
@@ -27,46 +27,37 @@ public class Mailer {
 			}
 		});
 		// compose message
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			String[] toList = to.split(",");
-			Address[] addresses = new Address[toList.length];
-			int i = 0;
-			for (String t : toList) {
-				addresses[i++] = new InternetAddress(t);
-			}
-			message.addRecipients(Message.RecipientType.TO, addresses);
-			message.setSubject(sub);
 
-			// Create MimeBodyPart object and set your message text
-			BodyPart messageBodyPart1 = new MimeBodyPart();
-			messageBodyPart1.setText(msg);
-
-			// Create Multipart object and add MimeBodyPart objects to this object
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(messageBodyPart1);
-
-			for (String pdf : pdfs.split(",")) {
-				// Create new MimeBodyPart object and set DataHandler object to this object
-				MimeBodyPart messageBodyPart2 = new MimeBodyPart();
-				DataSource source = new FileDataSource(pdfPath + File.separator + pdf);
-				messageBodyPart2.setDataHandler(new DataHandler(source));
-				messageBodyPart2.setFileName(source.getName());
-				multipart.addBodyPart(messageBodyPart2);
-
-			}
-
-			// Set the multiplart object to the message object
-			message.setContent(multipart);
-
-			// Send message
-			Transport.send(message);
-			System.out.println("Emails sent successfuly.");
-
-		} catch (MessagingException ex) {
-			ex.printStackTrace();
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));		
+		Address[] addresses = new Address[to.length];
+		int i = 0;
+		for (String t : to) {
+			addresses[i++] = new InternetAddress(t);
 		}
+		message.addRecipients(Message.RecipientType.TO, addresses);
+		message.setSubject(sub);
 
+		// Create MimeBodyPart object and set your message text
+		BodyPart messageBodyPart1 = new MimeBodyPart();
+		messageBodyPart1.setText(msg);
+
+		// Create Multipart object and add MimeBodyPart objects to this object
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(messageBodyPart1);
+
+		// Create new MimeBodyPart object and set DataHandler object to this object
+		MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+		DataSource source = new FileDataSource(pdfPath + File.separator + pdf);
+		messageBodyPart2.setDataHandler(new DataHandler(source));
+		messageBodyPart2.setFileName(source.getName());
+		multipart.addBodyPart(messageBodyPart2);
+
+		// Set the multiplart object to the message object
+		message.setContent(multipart);
+
+		// Send message
+		Transport.send(message);
+		//System.out.println("Email sent to " + to);
 	}
 }
--- src/main/java/com/mailer/SendMailSSL.java
+++ src/main/java/com/mailer/SendMailSSL.java
@@ -1,52 +1,79 @@
 package com.mailer;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class SendMailSSL {
 	public static void main(String[] args) {
-		
-		System.out.println("Usage:");
-		System.out.println("java -jar MailerUtil.jar <<from-gmail-address>> <<gmail-password>> <<subject>> <<pdfs-path>>");
-		System.out.println("Please note that all the pdfs in maillist.txt should be present in the pdf path given.");
-		System.out.println("This utility expects maillist.txt and mailbody.txt in same folder as MailerUtil.jar");
-		
-		if(args.length < 4) {
-			System.out.println("Please check usage..");
+
+		if (args.length < 5) {
+			System.out.println("Not Enough arguments to run. Please check usage.");
+			System.out.println("Usage:");
+			System.out.println(
+					"java -jar MailerUtil.jar <<from-gmail-address>> <<gmail-password>> <<as-of-date>> <<revert-by-date>> <<path>>");
 			System.exit(1);
 		}
 		String from = args[0];
 		String password = args[1];
-		String subject = args[2];
-		String pdfPath = args[3];
-		
-		String mailList = "maillist.txt";
-		String mailbody = "mailbody.txt";
-		StringBuilder body = new StringBuilder();
+		String asOfDate = args[2];
+		String revertByDate = args[3];
+		String path = args[4];
 
-		try (Stream<String> stream = Files.lines(Paths.get(mailbody))) {
+		File inputCsv = new File("input.csv");
 
-            stream.forEach(s -> {
-            	body.append(s + "\n");
-            });
+		List<List<String>> records = new ArrayList<>();
+		try (Scanner scanner = new Scanner(inputCsv);) {
+			while (scanner.hasNextLine()) {
+				records.add(getRecordFromLine(scanner.nextLine()));
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Problem in reading input csv from path " + inputCsv);
+			e.printStackTrace();
+			System.exit(1);
+		}
+		int counter = 0;
+		for (List<String> rec : records) {
+			if (counter++ == 0) {
+				continue;
+			}
+			System.out.println("Processing record for company: " + rec.get(0).trim());
+			String pdf = rec.get(0).trim() + ".pdf";
+			String balance = rec.get(1).trim();
+			String[] toEmails = rec.get(2).trim().split(";");
+			String sub = "Outstanding Balance Confirmation as of " + asOfDate;
+			String msg = "Dear Concern,\r\n" + "\r\n" + "Greetings from Tanmik Services..\r\n" + "\r\n"
+					+ "As per our books, there is an outstanding balance of Rs." + balance + " as of " + asOfDate
+					+ ", please refer to the attached ledger from our book.\r\n"
+					+ "Please reconcile and let us know if there are any concerns. \r\n" + "\r\n"
+					+ "Do ensure to revert by " + revertByDate
+					+ ", otherwise, we will consider the amount payable as our book stands correct.\r\n" + "\r\n"					
+					+ "Regards \r\n"
+					+ "Gurpreet Singh Sabharwal \r\n"
+					+ "9711722208 \r\n";
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //read file into stream, try-with-resources
-        try (Stream<String> stream = Files.lines(Paths.get(mailList))) {
+			try {
+				Mailer.send(from, password, toEmails, sub, msg, pdf, path);
+			} catch (Exception e) {
+				System.out.println("Email could not be sent to " + toEmails + ", Company: " + rec.get(0)
+						+ " Exception: " + e.getMessage());
+			}
 
-            stream.forEach(s -> {
-            	String[] parts = s.split(":");
-            	String to = parts[0];
-            	String pdfs = parts[1];
-            	Mailer.send(from, password, to, subject, body.toString(), pdfs, pdfPath);
-            });
+		}
+		System.out.println("Total records processed: " + (records.size() - 1));
+		System.out.println("All records processed successfuly!");
+	}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }		
+	private static List<String> getRecordFromLine(String line) {
+		List<String> values = new ArrayList<String>();
+		try (Scanner rowScanner = new Scanner(line)) {
+			rowScanner.useDelimiter(",");
+			while (rowScanner.hasNext()) {
+				values.add(rowScanner.next());
+			}
+		}
+		return values;
 	}
 }
